60620668de60fbfdc43fc0e28a678553fd6b8892
Gabrielė Dilytė <GDI@tiatechnology.com>
2021-09-28 21:45:15 +0300

    added additional options

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const listElement = document.querySelector(".guestList ol");
 let guestList = [];
 
 function updateList() {
-  listElement.innerHTML = "";
+  listElement.innerHTML = null;
   guestList.forEach((element) => {
     const listItem = document.createElement("li");
     listItem.textContent = element;
@@ -15,6 +15,9 @@ function updateList() {
 allButtons.forEach((element) => {
   element.addEventListener("click", (event) => {
     const guestName = document.querySelector("#guestName").value;
+    const removeFrom = Number(document.querySelector("#from").value);
+    const removeTo = Number(document.querySelector("#to").value);
+    const addAt = Number(document.querySelector("#at").value);
 
     switch (event.target.id) {
       case "addEnd":
@@ -35,6 +38,21 @@ allButtons.forEach((element) => {
         break;
       case "reverse":
         guestList.reverse();
+        break;
+      case "removeFrom":
+        if (removeTo) {
+          guestList.splice(removeFrom - 1, removeTo - removeFrom);
+        } else {
+          guestList.splice(removeFrom - 1, 1);
+        }
+      case "addAt":
+        guestList.splice(addAt - 1, 0, guestName);
+        break;
+      case "moveFirst":
+        guestList.push(guestList.splice(0, 1));
+        break;
+      case "moveLast":
+        guestList.unshift(guestList.splice(-1));
         break;
       default:
         break;
